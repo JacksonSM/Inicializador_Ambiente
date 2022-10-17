7f60dc2428edf23d76d741a661faaca0b02a81bb
JacksonSM <jacksons.m2018@gmail.com>
2022-10-17 18:36:48 -0300

    Salvando no arquivo os dados do ambiente.

---
+++ InicializadorAmbiente/Data/AmbienteDao.cs
@@ -0,0 +1,22 @@
+﻿using InicializadorAmbiente.Models;
+using Newtonsoft.Json;
+
+namespace InicializadorAmbiente.Data;
+public class AmbienteDao
+{
+    private static string Caminho = Application.UserAppDataPath;
+    private const string NOME_AMBIENTE = "config_ambiente.txt";
+    public static async Task SalvarAmiente(Ambiente ambiente) 
+    { 
+        var ambienteSerializado = JsonConvert.SerializeObject(ambiente);
+
+        string folder = Caminho;
+        string arquivo = Path.Combine(folder, NOME_AMBIENTE);
+        
+        Directory.CreateDirectory(folder);
+
+        File.Create(arquivo).Dispose();
+
+        await File.WriteAllTextAsync(arquivo, ambienteSerializado);
+    }
+}
